60175ac391884ddd17c0a42636ac487cc67ef237
chaitapatel <chaitapatel2789@gmail.com>
2026-09-22 11:19:22 +0530

    Add files via upload

---
+++ project_6output.py
@@ -0,0 +1,79 @@
+#WELCOME TO PERSONAL JOURNAL MANAGER!
+#1. Add a New Entry
+#2. View All Entries
+#. Search for an Entry
+#4. Delete All Entries
+#5. Exit
+#----------------------------------------
+#Please select an option: 1
+#Enter your journal entry: today was a good day.i learned about python today.
+#Entry added successfully!
+#WELCOME TO PERSONAL JOURNAL MANAGER!
+#1. Add a New Entry
+#2. View All Entries
+#3. Search for an Entry
+#4. Delete All Entries
+#5. Exit
+#----------------------------------------
+#Please select an option: 2
+
+#Your Journal Entries:
+#----------------------------------------
+#[2026-09-22 11:12:19]
+#today was a good day.i learned about python today.
+#----------------------------------------
+
+#WELCOME TO PERSONAL JOURNAL MANAGER!
+#1. Add a New Entry
+#2. View All Entries
+#3. Search for an Entry
+#4. Delete All Entries
+#5. Exit
+#----------------------------------------
+#Please select an option: 3
+#Enter a keyword or date to search: good day
+
+#Matching Entries:
+#----------------------------------------
+#[2026-09-22 11:12:19]
+#today was a good day.i learned about python today.
+#----------------------------------------
+
+#WELCOME TO PERSONAL JOURNAL MANAGER!
+#1. Add a New Entry
+#2. View All Entries
+#3. Search for an Entry
+#4. Delete All Entries
+#5. Exit
+#----------------------------------------
+#Please select an option: 4
+#Are you sure you want to delete all entries? (yes/no): yes
+#All journal entries have been deleted.
+#WELCOME TO PERSONAL JOURNAL MANAGER!
+#1. Add a New Entry
+#2. View All Entries
+#3. Search for an Entry
+#4. Delete All Entries
+#5. Exit
+#----------------------------------------
+#Please select an option: 2
+#Error: The journal file does not exist.
+#Please add a new entry first.
+#WELCOME TO PERSONAL JOURNAL MANAGER!
+#1. Add a New Entry
+#2. View All Entries
+#3. Search for an Entry
+#4. Delete All Entries
+#5. Exit
+#----------------------------------------
+#Please select an option: 6
+#Invalid option. Please select a valid option.
+#WELCOME TO PERSONAL JOURNAL MANAGER!
+#1. Add a New Entry
+#2. View All Entries
+#3. Search for an Entry
+#4. Delete All Entries
+#5. Exit
+#----------------------------------------
+#Please select an option: 5
+#Thank you for using Personal Journal Manager.Goodbye!
